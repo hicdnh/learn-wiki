4432ff6d5e1e4fb1324559d2e3c24134911aa89c
hicdnh <hicdnh@163.com>
2021-07-18 22:24:35 +0800

    日志格式修改，添加自定义的日志banner

--- src/main/java/com/example/wiki/WikiApplication.java
+++ src/main/java/com/example/wiki/WikiApplication.java
@@ -1,13 +1,24 @@
 package com.example.wiki;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import sun.rmi.runtime.Log;
+
 
 @SpringBootApplication
 public class WikiApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(WikiApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(WikiApplication.class, args);
+//		SpringApplication.run(WikiApplication.class, args);
+		SpringApplication app = new SpringApplication(WikiApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		logger.info("启动成功");
+		logger.info(String.format("启动端口:  %s", env.getProperty("server.port")));
 	}
 
 }
